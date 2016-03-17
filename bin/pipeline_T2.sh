1d2250ef0d5735d21cff56cb4b2a884c21b94ecf
Tristan Glatard <glatard@creatis.insa-lyon.fr>
2016-03-17 18:50:14 +0100

    Added bash wrapper script for output file bundling

---
+++ bin/pipeline_T2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function die {
+  echo $1
+  exit 1
+}
+
+if [ $# != 4 ]
+then
+  echo "usage: $0 <nifti_image> <json_file> <threshold_value> <output_file_name>"
+  exit 1
+fi
+
+OUTPUT_FILE=$4
+
+./run_pipeline_T2.sh $1 $2 $3 || die "Pipeline execution failed!"
+
+zip -r $4 *-T2_Error.nii *-M0_Error.nii *-T2map.nii *-M0map.nii || die "Cannot zip output files!"
